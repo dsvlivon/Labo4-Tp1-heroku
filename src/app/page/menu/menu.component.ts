9aa306e5fb049352ef4acef48120d452da0b01be
Vizgarra Livon <daniel.vizgarralivon@kcc.com>
2022-04-11 09:46:27 -0300

    agrego el c footer

--- src/app/page/menu/menu.component.ts
+++ src/app/page/menu/menu.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  linkedin:string = "https://www.linkedin.com/in/dsvlivon/";
+  linkPic:string = "https://media-exp1.licdn.com/dms/image/C560BAQHaVYd13rRz3A/company-logo_100_100/0/1638831589865?e=2147483647&v=beta&t=fi3iyTgSAogCMgSmAy_DeyogJxzo38RVBK0mcEuSpc8";
+  github:string = "https://github.com/dsvlivon";
+  gitPic:string = "https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU";
   constructor() { }
 
   ngOnInit(): void {
